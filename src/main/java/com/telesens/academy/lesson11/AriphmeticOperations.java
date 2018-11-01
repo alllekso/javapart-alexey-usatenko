c63f402ea82275914bccfdbd3730db86ad7aaabf
Allekso <ajjekso@gmail.com>
2018-11-01 08:27:31 +0200

    lesson 11

---
+++ src/main/java/com/telesens/academy/lesson11/AriphmeticOperations.java
@@ -0,0 +1,7 @@
+package com.telesens.academy.lesson11;
+
+import java.math.BigDecimal;
+
+public interface AriphmeticOperations {
+BigDecimal div(BigDecimal x1, BigDecimal x2) throws DivisionByZero
+}
