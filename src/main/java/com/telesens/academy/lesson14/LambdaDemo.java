27d2f4afd6ffb4727670378664b19d5fb9bea306
Allekso <ajjekso@gmail.com>
2018-11-13 07:38:12 +0200

    lesson15

--- src/main/java/com/telesens/academy/lesson14/LambdaDemo.java
+++ src/main/java/com/telesens/academy/lesson14/LambdaDemo.java
@@ -1,4 +1,17 @@
-package com.telesens.academy.lesson14;
-
-public class LambdaDemo {
-}
+//package com.telesens.academy.lesson14;
+//
+//import java.util.function.Function;
+//
+//import static com.telesens.academy.lesson14.lambda.LambdaDemo.doSomethingWithString;
+//
+//public class LambdaDemo {
+//    String str = "Hello World";
+//    String res = doSomethingWithString(str, s->s.toUpperCase());
+//    String res2 = doSomethingWithString(str, String::toLowerCase);
+//    Function<String, String> f1 = s->s.toUpperCase();
+//    Function<String, String> f2 = String::toLowerCase;
+//    String res3 = doSomethingWithString(str, f2);
+////         System.out.println(res);
+////         System.out.println(res2);
+////    // TODO
+//}
